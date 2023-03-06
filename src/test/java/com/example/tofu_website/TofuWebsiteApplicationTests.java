7f9cfad050830c8107e5361058cb6a449b2fb7bf
Aileena <aile0074@stud.kea.dk>
2023-03-06 13:36:32 +0100

    haha

---
+++ src/test/java/com/example/tofu_website/TofuWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tofu_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TofuWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
