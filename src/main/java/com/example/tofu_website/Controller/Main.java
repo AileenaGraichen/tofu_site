e5f563be44ac6a5d9942da0ea6c435d245b839d4
Aileena <aile0074@stud.kea.dk>
2023-03-06 14:06:33 +0100

    haha haha

--- src/main/java/com/example/tofu_website/Controller/Main.java
+++ src/main/java/com/example/tofu_website/Controller/Main.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class Main {
     @GetMapping("/")
     public String home(){
-        return "home";
+        return "homx";
     }
 }
